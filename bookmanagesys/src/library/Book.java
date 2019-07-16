f21017a1ee6e4dbb350c5a77f3f2ef63f3e8af5e
jasmhusc <15927162908@163.com>
2019-07-16 22:47:01 +0800

    add moduals

--- bookmanagesys/src/library/Book.java
+++ bookmanagesys/src/library/Book.java
@@ -7,4 +7,36 @@ public class Book {
     private String author;
     private Date date;
 
+    public Book() {
+    }
+
+    public Book(String bookname, String author, Date date) {
+        this.bookname = bookname;
+        this.author = author;
+        this.date = date;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- bookmanagesys/src/library/Library.java
+++ bookmanagesys/src/library/Library.java
@@ -1,5 +1,16 @@
 package library;
 
+import java.util.ArrayList;
+
 public class Library {
 
+    private ArrayList<Book> lib;
+
+    public Library() {
+        lib = new ArrayList<>();
+    }
+
+    public void add(Book book){
+
+    }
 }
